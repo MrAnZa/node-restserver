413c7af940157647ed09eec68ebaa23bfc9f6095
Andres Zamora <andreszamora1998@gmail.com>
2020-04-19 18:51:05 -0500

    CRUD Categorias GETs & DELETE

--- server/routes/categoria.js
+++ server/routes/categoria.js
@@ -1,17 +1,53 @@
 const express = require('express');
 
-let { verificaToken } = require('../middlewares/autenticacion');
+let { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 let app = express();
 
 let Categoria = require('../models/categoria');
 
 app.get('/categoria', (req, res) => {
+    Categoria.find({})
+        .exec((err, categorias) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
 
+            res.json({
+                ok: true,
+                categorias
+            });
+        })
 })
 
 app.get('/categoria/:id', (req, res) => {
 
+    let id = req.params.id;
+
+    Categoria.findById(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoriaDB) {
+            return res.status(500).json({
+                ok: false,
+                err: {
+                    message: 'El ID no es Correcto'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        })
+    })
 })
 
 app.post('/categoria', verificaToken, (req, res) => {
@@ -41,7 +77,7 @@ app.post('/categoria', verificaToken, (req, res) => {
     });
 })
 
-app.put('/categoria/:id', (req, res) => {
+app.put('/categoria/:id', verificaToken, (req, res) => {
     let id = req.params.id;
     let body = req.body;
 
@@ -69,7 +105,27 @@ app.put('/categoria/:id', (req, res) => {
     })
 })
 
-app.delete('/categoria/:id', (req, res) => {
-
+app.delete('/categoria/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
+    let id = req.params.id;
+    Categoria.findByIdAndRemove(id, (err, categoriaDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!categoriaDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El id no existe'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            message: 'Categoria Borrada'
+        });
+    })
 })
 module.exports = app;
